f5ab94d6b06e6882617cf0fc9ff36bc91b3d07a1
alexn34-n <alexn3469@mail.ru>
2021-09-28 13:58:01 +0300

    Initial commit

---
+++ src/main/java/com/alexn/spring/springboot/spring_data_jpa/student_data_jpa_ht/StudentDataJpaHtApplication.java
@@ -0,0 +1,13 @@
+package com.alexn.spring.springboot.spring_data_jpa.student_data_jpa_ht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataJpaHtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDataJpaHtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexn/spring/springboot/spring_data_jpa/student_data_jpa_ht/StudentDataJpaHtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexn.spring.springboot.spring_data_jpa.student_data_jpa_ht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataJpaHtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
